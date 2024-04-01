1ae7ff4b03009e9531c2046c0300674eb61033a6
tticehurst <shnopy319@gmail.com>
2024-04-01 16:13:35 +0100

    finished

--- MMM-CalendarDisplayMonthOverview.js
+++ MMM-CalendarDisplayMonthOverview.js
@@ -3,11 +3,49 @@ Module.register("MMM-CalendarDisplayMonthOverview", {
 
   start() {
     this.events = [];
+
+    this.nunjucksEnvironment().addFilter("toDayNumber", (date) => {
+      return new Date(date).getDate();
+    });
   },
 
   notificationReceived(notification, pl) {
     if (notification === "CAL-DISPLAY-EVENTS") {
-      console.log(notification, pl);
+      let splitArrayIntoSets = (array, setSize) => {
+        let sets = [];
+        for (let i = 0; i < array.length; i += setSize) {
+          sets.push(array.slice(i, i + setSize));
+        }
+        return sets;
+      };
+
+      let segments = splitArrayIntoSets(pl.data, 7);
+      let max = Math.max(...segments.flatMap((x) => x.map((e) => e.events)));
+
+      this.today = pl.today;
+      this.max = max;
+      this.month = new Date(pl.today).toLocaleString("default", {
+        month: "long"
+      });
+
+      segments = segments.map((seg) =>
+        seg.map((eventSegment) => {
+          let gradValue = Math.round((eventSegment.events / max) * 100);
+          let gradColour =
+            eventSegment.events > 0
+              ? `hsl(286,${gradValue}%,${60 - gradValue / 3}%)`
+              : "";
+
+          return {
+            ...eventSegment,
+            colour: eventSegment.day !== pl.today ? gradColour : ""
+          };
+        })
+      );
+
+      this.segments = segments;
+
+      this.updateDom(300);
     }
   },
 
@@ -17,5 +55,14 @@ Module.register("MMM-CalendarDisplayMonthOverview", {
 
   getStyles() {
     return ["CalendarDisplayMonthOverview.css"];
+  },
+
+  getTemplateData() {
+    return {
+      segments: this.segments,
+      today: this.today,
+      max: this.max,
+      month: this.month
+    };
   }
 });
